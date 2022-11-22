d84afa00f398d0c232cea4fae0aab598941e2097
Guille Zorron <gzorron@xmartlabs.com>
2022-11-21 22:25:29 -0300

    dummy login endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { express, Router } from 'express'
+import express from 'express'
 import cors from 'cors';
-import { login } from './controllers/session-controller';
-// import { Router, Request, Response } from "express";
+import { login } from './controllers/sessions-controller.js';
+import { Router } from "express";
 
 const app = express()
 const routes = Router();
