6031a1a5872a79ef1b7f7ccbbed1dae21c41560c
Guille Zorron <gzorron@xmartlabs.com>
2022-11-21 23:21:41 -0300

    user params from request

--- src/controllers/sessions-controller.js
+++ src/controllers/sessions-controller.js
@@ -1,5 +1,6 @@
-const login = function() {
-    console.log('login');
+const login =  ({ body }, res) => {
+    console.log(body);
+    res.status(200).json({"user": body.user, "pass": body.password})
 };
 
 export { login };
